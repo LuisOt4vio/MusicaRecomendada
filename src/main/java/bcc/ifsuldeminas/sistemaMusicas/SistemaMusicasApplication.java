6858055690999b8950a1fead249f5cb15e1788f8
APSS892 <anapaulasantossilva892@gmail.com>
2025-01-04 00:07:44 -0300

    Inicio do sitema de recomendção de musicas

---
+++ src/main/java/bcc/ifsuldeminas/sistemaMusicas/SistemaMusicasApplication.java
@@ -0,0 +1,13 @@
+package bcc.ifsuldeminas.sistemaMusicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMusicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMusicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bcc/ifsuldeminas/sistemaMusicas/SistemaMusicasApplicationTests.java
@@ -0,0 +1,13 @@
+package bcc.ifsuldeminas.sistemaMusicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMusicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
